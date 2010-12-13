193dd2fd1d0fe624a859ac0d240bf4d5d9e4867b
tk0miya <i.tkomiya@gmail.com>
2010-12-13 18:19:58 +0900

    * Implement apikey switcher for googlemaps

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 sys.path.insert(0, './distlib.zip')
 
@@ -15,18 +16,32 @@ app = Flask(__name__)
 app.debug = True
 
 
+def get_template_hash():
+    hash = {}
+
+    if os.environ.get('SERVER_SOFTWARE', '').startswith('Development'):
+        hash['apikey'] = 'ABQIAAAA4eql-YSnJdjJAh3TCsXYpRSVxpMzLo2MSr9OjI8XKMaS10WqSxRWW3eDU1q5dOIChUjN1E5L8yBE7w'
+    else:
+        hash['apikey'] = 'ABQIAAAASH-PWXJDalCiynGXOAga6BTc7Dug7_44qRm7yAN83pI9KXijgxSF1EgF11_vZuIk4GqCZZcMLHNwkQ'
+
+    return hash
+
+
 @app.route('/')
 def index():
     page = int(request.args.get('page', 0))
+    kwargs = get_template_hash()
 
     maps = Map.all().order('-id').fetch(PAGESIZE + 1, page * PAGESIZE)
+    kwargs['page'] = page
     if len(maps) == PAGESIZE + 1:
-        maps = maps[:PAGESIZE]
-        nextpage = True
+        kwargs['maps'] = maps[:PAGESIZE]
+        kwargs['nextpage'] = True
     else:
-        nextpage = False
+        kwargs['maps'] = maps
+        kwargs['nextpage'] = False
 
-    return render_template('list.html', maps=maps, page=page, nextpage=nextpage)
+    return render_template('list.html', **kwargs)
 
 
 @app.route('/list')
@@ -34,39 +49,45 @@ def index():
 def list(tag=None):
     mode = request.args.get('mode')
     page = int(request.args.get('page', 0))
+    kwargs = get_template_hash()
 
     query = Map.all().order('-id')
     if tag:
         query = query.filter("tag =", tag)
 
     maps = query.fetch(PAGESIZE + 1, page * PAGESIZE)
+    kwargs['page'] = page
     if len(maps) == PAGESIZE + 1:
-        maps = maps[:PAGESIZE]
-        nextpage = True
+        kwargs['maps'] = maps[:PAGESIZE]
+        kwargs['nextpage'] = True
     else:
-        nextpage = False
+        kwargs['maps'] = maps
+        kwargs['nextpage'] = False
 
-    return render_template('list.html', maps=maps, page=page, nextpage=nextpage, mode=mode)
+    return render_template('list.html', **kwargs)
 
 
 @app.route('/show_map/<int:map_id>')
 def show_map(map_id):
-    map = Map.all().filter("id =", map_id).get()
-    return render_template('show_map.html', map=map)
+    kwargs = get_template_hash()
+
+    kwargs['map'] = Map.all().filter("id =", map_id).get()
+    return render_template('show_map.html', **kwargs)
 
 
 @app.route('/show_large_map/<key>')
 def show_large_map(key):
+    kwargs = get_template_hash()
+
     if re.search('^\d+$', key):
-        map = Map.all().filter("id =", int(key)).get()
-        return render_template('show_large_map.html', map=map)
+        kwargs['map'] = Map.all().filter("id =", int(key)).get()
     elif re.search('^(\d+,)*\d+$', key):
-        map_id_list = ",".join(str(x) for x in key.split(','))
-        return render_template('show_large_map.html', map_id_list=map_id_list)
+        kwargs['map_id_list'] = ",".join(str(x) for x in key.split(','))
     else:
         map = Map.all().filter("tag =", key).fetch(1000)
-        map_id_list = ",".join(str(x.id) for x in map)
-        return render_template('show_large_map.html', map_id_list=map_id_list)
+        kwargs['map_id_list'] = ",".join(str(x.id) for x in map)
+
+    return render_template('show_large_map.html', **kwargs)
 
 
 @app.route('/xml/<map_id>')
